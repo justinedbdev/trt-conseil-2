b2fa50ac8ea74a62af162a75b18b234c72a25329
Justine Debard <justine_debard@yahoo.com>
2023-08-29 16:03:40 +0200

    Create Apply entity

---
+++ src/Entity/Apply.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ApplyRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ApplyRepository::class)]
+class Apply
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $isValidated = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function isIsValidated(): ?bool
+    {
+        return $this->isValidated;
+    }
+
+    public function setIsValidated(?bool $isValidated): static
+    {
+        $this->isValidated = $isValidated;
+
+        return $this;
+    }
+}

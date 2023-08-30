194900d32e46beb903d44c71b1ac29c2222086a6
Justine Debard <justine_debard@yahoo.com>
2023-08-30 14:14:58 +0200

    Create Registration form

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,10 +4,15 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -17,33 +22,75 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email',  EmailType::class, [
+                'label' => 'Votre email',
+                'required' => true,
+                'attr' => [
+                    'class' => 'mt-2',
+                    'placeholder' => 'Entrez votre email'
+                ],
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez saisir un email valide !']),
+                    new Email(['mode' => 'html5', 'message' => 'L\'email : {{ value }} n\'est pas valide']),
+                ]
+            ])
             ->add('agreeTerms', CheckboxType::class, [
+                'label' => 'Accepter les conditions d\'utilisation',
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Vous devez accepter les conditions d\'utilisation pour pouvoir vous inscrire.',
                     ]),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
+                'invalid_message' => 'Les mots de passe doivent être identiques',
+                'options' => [
+                    'attr' => [
+                        'class' => 'password-field',
+                        'autocomplete' => 'new-password',
+                        'placeholder' => 'Entrez votre mot de passe'
+                    ]
                 ],
+                'required' => true,
+                'first_options'  => [
+                    'label' => 'Entrez votre mot de passe',
+                    'constraints' => [
+                        new NotBlank(['message' => 'Veuillez saisir un mot de passe.']),
+                        new Length(['min' => 6, 'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères', 'max' => 4096,]),
+                    ]],
+                'second_options' => [
+                    'label' => 'Validez votre mot de passe',
+                    'constraints' => [
+                        new NotBlank(['message' => 'Veuillez saisir un mot de passe.']),
+                        new Length(['min' => 6, 'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères', 'max' => 4096,]),
+                    ]],
             ])
-        ;
+            ->add('roles', ChoiceType::class, [
+                'label' => 'Vous êtes' ,
+                'expanded' => true,
+                'choices' => [
+                    'Candidat' => 'ROLE_CANDIDAT',
+                    'Recruteur' => 'ROLE_RECRUITER',
+                ],
+                'multiple' => false,
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(['message' => 'Veuillez choisir une proposition.'])
+                ]
+            ]);
+            
+        $builder->get('roles')
+        ->addModelTransformer(new CallbackTransformer(
+            function ($rolesAsArray): string {
+                return implode(',', $rolesAsArray);
+            },
+            function ($rolesAsString): array {
+                return explode(',', $rolesAsString);
+            }
+        ));
     }
 
     public function configureOptions(OptionsResolver $resolver): void

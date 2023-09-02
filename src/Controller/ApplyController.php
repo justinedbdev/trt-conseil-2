ee9beb5db14d31368c007132bdb1c98d3f3a3f2d
Justine Debard <justine_debard@yahoo.com>
2023-09-02 13:06:16 +0200

    Add Secrurity access to Apply Controller + clean code

--- src/Controller/ApplyController.php
+++ src/Controller/ApplyController.php
@@ -12,7 +12,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
+#[Security("is_granted('ROLE_RECRUITER') and is_granted('ROLE_CANDIDAT')")]
 #[Route('/candidature')]
 class ApplyController extends AbstractController
 {
@@ -44,17 +46,15 @@ class ApplyController extends AbstractController
         $candidats = $user->getCandidats();
 
         if (!$this->getUser()) {
-            $this->addFlash('danger', 'Vous devez être connecté.');
+            $this->addFlash('danger', 'Vous devez être connecté pour accéder à cette page.');
             return $this->redirectToRoute('app_login');
         }
 
         $apply = new Apply();
-        //Recup id_joboffer
         $jobOffer = $jobOfferRepo->find($id);
         $apply->setJobOffer($jobOffer);
         $jobOffer->addApply($apply);
 
-        //lier candidate à apply
         foreach ($candidats as $candidat) {
             $idCandidat = $candidat->getId();
             $candidat = $candidatRepo->find($idCandidat);
@@ -72,7 +72,7 @@ class ApplyController extends AbstractController
             $this->em->flush();
             $this->addFlash('success', 'Votre candidature a bien été enregistrée.');
 
-            return $this->redirectToRoute('jobOffer');
+            return $this->redirectToRoute('app_jobOffer');
         }
     }
 }
